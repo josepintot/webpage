61946af93870d8b197edb8c19779f52ff81b372a
jose <45467659+josepintot@users.noreply.github.com>
2019-01-29 13:20:49 -0500

    Add files via upload

---
+++ portafolio web page/test.js
@@ -0,0 +1,34 @@
+
+function testCompare()
+{
+numbersA = {};
+numbersB = {};
+
+int j = 0;
+int i = 0;
+a = 0;
+b = 0;
+var isTheSame = false
+for (i=0;i<1000;i++)
+    {
+    a = a + 1;
+    for (j = 0; j<1000; j++)
+         {
+         b = b +1
+		 isTheSame = compareObjects(a,b)
+         if (!isTheSame == (compareObjects(a,b))
+             {
+             console.log("Test Failed")
+			 
+             }
+         }
+    b = 0;
+    }
+}
+
+
+
+
+Collapse 
+
+Message Input
